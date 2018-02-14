7c700532592aa3c8386fbff539bba367fb694fdf
200360407 <200360407.student.georgianc.on.ca>
2018-02-13 21:07:30 -0500

    lab1

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get: Products/Details?genre=[value]
+        public ActionResult Details(string genre)
+        {
+            ViewBag.Genre = genre;
+            return View();
+        }
+    }
+}
